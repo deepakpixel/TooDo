7890b279ba5f1b700035aa8a9e4d2623cf975048
deepakpixel <gtx.addicted@gmail.com>
2021-07-09 17:05:47 +0530

    Testing json-server on heroku

--- json-server.js
+++ json-server.js
@@ -2,9 +2,11 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3000;
+
+const port = process.env.PORT || 3333;
 
 server.use(middlewares);
 server.use(router);
-
-server.listen(port);
+server.listen(port, () => {
+  console.log('JSON Server is running');
+});
