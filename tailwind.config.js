7fccd662b746cab9a5285b69469f50265c34f1ba
deepakpixel <gtx.addicted@gmail.com>
2021-08-20 19:59:46 +0530

    purgeCSS is black magic 🔥

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,14 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  purge: {
+    enabled: true,
+    content: ['src/**/*.js', 'src/**/*.jsx', 'public/**/*.html'],
+    options: {
+      safelist: [
+        /.*^(border|bg|text|ring).*/,
+        /.*^(focus:border|hover:bg|focus:bg|hover:text|focus:ring).*/,
+      ],
+    },
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
